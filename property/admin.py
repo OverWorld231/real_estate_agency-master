25ec3fb25334960773d4ef39d7c67c12e5dcdd99
OverWorld231 <107308919+OverWorld231@users.noreply.github.com>
2026-04-05 14:10:47 +0300

    Initial commit

---
+++ property/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import Flat
+from .models import Complaint
+from .models import Owner
+class AdminInline(admin.TabularInline):
+    model = Owner.owner_flats.through
+    raw_id_fields = ("owner",)
+
+@admin.register(Flat)
+class SearchAdmin(admin.ModelAdmin):
+    search_fields = ('owner','town','address')
+    readonly_fields = ["created_at"]
+    list_display = ["address","price","new_building","construction_year","town", "owners_phonenumber", "owner_pure_phone"]
+    list_editable = ["new_building"]
+    list_filter = ["new_building", "rooms_number","has_balcony","construction_year","town"]
+    raw_id_fields = ("liked_by",)
+    exclude = ("flats_in_ownership",)
+    inlines = [AdminInline]
+
+@admin.register(Complaint)
+class ComplaintAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user","flat",)
+    
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ("owner_flats",)
---
+++ property/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Flat(models.Model):
+    owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    created_at = models.DateTimeField(
+        'Когда создано объявление',
+        default=timezone.now,
+        db_index=True)
+    owner_pure_phone = PhoneNumberField("Нормализованный номер владельца",null=True,  blank=True, region="RU")
+    new_building = models.BooleanField("Новостройка", null=True, blank=False)
+    liked_by = models.ManyToManyField(User, related_name="liked_flats", verbose_name="Кто лайкнул", blank=True)
+    
+    def save(self, *args, **kwargs):
+        if self.construction_year and self.construction_year >= 2015:
+            self.new_building = True
+        else:
+            self.new_building = False
+
+        super().save(*args, **kwargs)
+
+    description = models.TextField('Текст объявления', blank=True)
+    price = models.IntegerField('Цена квартиры', db_index=True)
+
+    town = models.CharField(
+        'Город, где находится квартира',
+        max_length=50,
+        db_index=True)
+    town_district = models.CharField(
+        'Район города, где находится квартира',
+        max_length=50,
+        blank=True,
+        help_text='Чертаново Южное')
+    address = models.TextField(
+        'Адрес квартиры',
+        help_text='ул. Подольских курсантов д.5 кв.4')
+    floor = models.CharField(
+        'Этаж',
+        max_length=3,
+        help_text='Первый этаж, последний этаж, пятый этаж')
+
+    rooms_number = models.IntegerField(
+        'Количество комнат в квартире',
+        db_index=True)
+    living_area = models.IntegerField(
+        'количество жилых кв.метров',
+        null=True,
+        blank=True,
+        db_index=True)
+
+    has_balcony = models.BooleanField('Наличие балкона', null=True, blank=True, db_index=True)
+    active = models.BooleanField('Активно-ли объявление', db_index=True)
+    construction_year = models.IntegerField(
+        'Год постройки здания',
+        null=True,
+        blank=True,
+        db_index=True)
+
+    def __str__(self):
+        return f'{self.town}, {self.address} ({self.price}р.)'
+
+class Complaint(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, verbose_name="Кто написал жалобу", null=True, related_name="complaints")
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name="На какую квартиру жалоба", related_name="complaints")
+    text = models.TextField("Текст жалобы")
+
+    def __str__(self):
+        return f"Жалоба от {self.user}"
+    
+class Owner(models.Model):
+    owner = models.CharField('ФИО владельца', max_length=200)
+    owners_phonenumber = models.CharField('Номер владельца', max_length=20)
+    owner_pure_phone = PhoneNumberField("Нормализованный номер владельца",null=True,  blank=True, region="RU")
+    owner_flats = models.ManyToManyField(Flat, blank=True, related_name="owner_by", verbose_name="Квартиры в собственности")
+    
+
+    def __str__(self):
+        return self.owner
+
